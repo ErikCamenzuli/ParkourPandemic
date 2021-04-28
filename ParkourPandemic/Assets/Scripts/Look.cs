e8cfea0d41e0e4488b24550b4deeeedbadf1630b
Erik <erik.camenzuli@hotmail.com>
2021-04-28 16:08:08 +1000

    Added Scripts + comments

---
+++ ParkourPandemic/Assets/Scripts/Look.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public Transform playerLook;
+    public float mouseSens = 10f;
+
+    private float x = 0;
+    private float y = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += -Input.GetAxis("Mouse Y") * mouseSens;
+        y += Input.GetAxis("Mouse X") * mouseSens;
+
+        x = Mathf.Clamp(x, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        playerLook.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else if(Cursor.lockState == CursorLockMode.None)
+                Cursor.lockState = CursorLockMode.Locked;
+
+        }
+
+    }
+}
---
+++ ParkourPandemic/Assets/Scripts/Movement.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Transform orientation;
+    public LayerMask groundCheck;
+    public LayerMask isWall;
+    
+    bool isGrounded;
+    bool isWallRunning;
+    bool wallRight;
+    bool wallLeft;
+
+    public float speed = 10f;
+    public float jump = 3f;
+    public float wallRunForce;
+    public float maxRunTimeWall;
+    public float maxSpeedWall;
+    public float cameraTiltMax;
+    public float cameraTiltWallRun;
+
+    public float climbSpeed;
+    public float climbWall;
+    public float distanceWallclimb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //getting the rigidbody
+        rb = GetComponent<Rigidbody>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///Summmary
+        ///Inputs for player movement on Horizontal and Vertical axis
+        ///GetAxisRaw for more "snapy" movement
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+        float y = Input.GetAxisRaw("Vertical") * speed;
+
+        //GroundCheck
+        //Creating a sphere check for a ground check and checking if the player is grounded
+        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, 
+                                                     transform.position.z), 0.4f, groundCheck);
+        //Jump
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
+        
+
+        Vector3 movePos = transform.right * x + transform.forward * y;
+        Vector3 newPos = new Vector3(movePos.x, rb.velocity.y, movePos.z);
+
+        rb.velocity = newPos;    
+
+        WallRun();
+        WallRunCheck();
+    }
+    /// <summary>
+    /// If inputs A or D are pressed, it will start the wall run
+    /// </summary>
+    private void WallRun()
+    {
+        if (Input.GetKey(KeyCode.D) && wallRight) 
+            WallRunStart();
+        if (Input.GetKey(KeyCode.A) && wallLeft) 
+            WallRunStart();
+    }
+    /// <summary>
+    /// Disabling the use of gravity & enables wall running
+    /// If the rigidbody magnitude is less than or equaled to the max speed
+    ///     add force to the orintation 
+    /// Checks for the wall if it's on the right or left
+    ///     add force to the orintation 
+    ///     else flip orintation if left
+    /// </summary>
+    private void WallRunStart()
+    {
+        rb.useGravity = false;
+        isWallRunning = true;
+
+        if(rb.velocity.magnitude <= maxSpeedWall)
+        {
+            rb.AddForce(orientation.forward * wallRunForce * Time.deltaTime);
+
+            if (wallRight)
+                rb.AddForce(orientation.right * wallRunForce / 5 * Time.deltaTime);
+            else
+                rb.AddForce(-orientation.right * wallRunForce / 5 * Time.deltaTime);
+        }
+
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private void WallRunStop()
+    {
+        rb.useGravity = true;
+        isWallRunning = false;
+
+    }
+
+    private void WallRunCheck()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, 1f, isWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, 1f, isWall);
+
+        if (!wallLeft && !wallRight) 
+            WallRunStop();
+
+    }
+
+    public void CanPlayerClimb()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, distanceWallclimb))
+        {
+            if(hit.transform.tag == "PlayerCanClimb")
+            {
+
+                if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.W))
+                {
+                    rb.velocity = Vector3.up;
+                }
+            }
+        }
+    }
+
+}
